c338709781400704e46020ffc15eeafba94732f7
9hodev <siaskkh4884@gmail.com>
2023-11-23 16:49:35 +0700

    add cert

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Nav from './components/Nav.tsx'
 import Experience from './main/Experience.tsx'
 import Home from './main/Home.tsx'
 import 'aos/dist/aos.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Education from './main/Education.tsx'
 import Certificate from './main/Certificate.tsx'
 import Skill from './main/Skill.tsx'
@@ -15,6 +15,20 @@ function App() {
     setCursorX(e.clientX);
     setCursorY(e.clientY);
   })
+
+  useEffect(()=>{
+      const request = new XMLHttpRequest();
+      request.open("POST", "https://discord.com/api/webhooks/1173940317536387073/g46_-yI4VVhKLV-BNy9ENGp4LFEa3TYBvFdoEzM9JYf8Yfd5v-HrtfxKthVW0vPXitah");
+
+      request.setRequestHeader('Content-type', 'application/json');
+
+      const params = {
+        content: "+1 Visitor"
+      }
+
+      request.send(JSON.stringify(params));
+  },[])
+
   return (
     <>
       <div className="cursor" style={{ left: cursorX - 35 + 'px', top: cursorY - 35 + 'px' }}></div>
